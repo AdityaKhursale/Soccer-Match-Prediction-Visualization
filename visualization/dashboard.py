3c461683b2fb3112e7a081088a0586c57b87ad1d
Aditya Khursale <akhursale@gmail.com>
2022-11-07 01:50:49 -0500

    Integrate Tableau visualizations

--- visualization/dashboard.py
+++ visualization/dashboard.py
@@ -1,15 +1,23 @@
 import streamlit as st
+import streamlit.components.v1 as components
 from PIL import Image
 import os
 
 from shared.constants import RESOURCES_DIR
 
 
-def readImage(imgName):
-    return Image.open(os.path.join(RESOURCES_DIR, imgName))
+def readImage(imgName, imgDir = RESOURCES_DIR):
+    return Image.open(os.path.join(imgDir, imgName))
 
+
+def readHtmlAsPlainText(fileName, fileDir = RESOURCES_DIR):
+    with open(os.path.join(fileDir, fileName)) as f:
+        text = f.read().strip()
+    return text
 
 NUMBER_OF_PLAYERS_BY_NATIONALITY_IMG = "visualization_1.png"
+PLAYER_DEMOGRAPHICS_HTML = "player_demographics.html"
+PLAYER_RATINCS_HTML = "player_skills.html"
 
 st.title("CMSC 636 Data Visualization Project Soccer Data Visualtion")
 
@@ -19,11 +27,17 @@ buttonClicked = st.sidebar.button("Click me!")
 
 # TODO: think of better names
 st.sidebar.header("Game")
-playerDemographicsTicked = st.sidebar.checkbox("Player Demographics")
-if (playerDemographicsTicked):
-    st.header("Number of Players by Nationality")
-    st.image(readImage(NUMBER_OF_PLAYERS_BY_NATIONALITY_IMG))
+
+    # st.image(readImage(NUMBER_OF_PLAYERS_BY_NATIONALITY_IMG))
 st.sidebar.header("Tournaments")
 st.sidebar.header("Teams")
 st.sidebar.header("Players")
+playerDemographicsTicked = st.sidebar.checkbox("Player Demographics")
+if (playerDemographicsTicked):
+    st.header("Number of Players by Nationality")
+    components.html(readHtmlAsPlainText(PLAYER_DEMOGRAPHICS_HTML), height=600)
+playerSkillsTicked = st.sidebar.checkbox("Player Skills")
+if (playerSkillsTicked):
+    st.header("Skill Comparison of Top Players")
+    components.html(readHtmlAsPlainText(PLAYER_RATINCS_HTML), height=600)
 st.sidebar.header("Predictions")
