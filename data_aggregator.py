2b97500fa883e63aa8f73157b8a0f0be720f1cbc
Aditya Khursale <akhursale@gmail.com>
2022-11-10 03:17:04 -0500

    Add Match Prediction

--- data_aggregator.py
+++ data_aggregator.py
@@ -1,9 +1,10 @@
 import copy
 import os
 import sqlite3
-from abc import abstractmethod
+import numpy as np
+import pandas as pd
 
-from pandas import DataFrame
+from abc import abstractmethod
 from shared.constants import DATASET_PATH, EUROPEAN_SOCCER_DATABASE
 from utils.db_helper import SqliteHelper
 
@@ -35,7 +36,69 @@ class LeagueDataHelper(DataHelper):
 
 
 class MatchDataHelper(DataHelper):
-    pass
+    @staticmethod
+    def filterMatchesBefore(matches, date, limit):
+        if (matches.shape[0] < limit):
+            limit = matches.shape[0]
+        return matches[matches.date < date].sort_values(
+            by="date", ascending=False).iloc[:limit, :]
+
+    @staticmethod
+    def filterMatchesByTeamApiId(matches, teamId):
+        return matches[(matches.home_team_api_id == teamId) |
+                       (matches.away_team_api_id == teamId)]
+
+    @staticmethod
+    def filterMatchesByOpponentsTeamIds(matches, team1Id, team2Id):
+        team1Matches = matches[
+            (matches.home_team_api_id == team1Id) &
+            (matches.away_team_api_id == team1Id)
+        ]
+        team2Matches = matches[
+            (matches.home_team_api_id == team2Id) &
+            (matches.away_team_api_id == team2Id)
+        ]
+        return pd.concat([team1Matches, team2Matches])
+
+    @staticmethod
+    def getGoalsByTeamId(matches, teamId):
+        return \
+        int(matches.home_team_goal[matches.home_team_api_id == teamId].sum()) + \
+        int(matches.away_team_goal[matches.away_team_api_id == teamId].sum())
+
+    @staticmethod
+    def getGoalsConceidedByTeamId(matches, teamId):
+        return \
+        int(matches.home_team_goal[matches.away_team_api_id == teamId].sum()) + \
+        int(matches.away_team_goal[matches.home_team_api_id == teamId].sum())
+
+    @staticmethod
+    def getWinsByTeamId(matches, teamId):
+        return \
+        int(
+            matches.home_team_goal[
+                (matches.home_team_api_id == teamId) &
+                (matches.home_team_goal > matches.away_team_goal)
+            ].count()
+        ) + \
+        int(
+            matches.away_team_goal[
+                (matches.away_team_api_id == teamId) &
+                (matches.away_team_goal > matches.home_team_goal)
+            ].count()
+        )
+
+    @staticmethod
+    def getMatchResult(match):
+        matchResult = pd.DataFrame()
+        matchResult.loc[0, "match_api_id"] = match.match_api_id
+        if match["home_team_goal"] > match["away_team_goal"]:
+            matchResult.loc[0, "label"] = "Win"
+        elif match["home_team_goal"] < match["away_team_goal"]:
+            matchResult.loc[0, "label"] = "Defeat"
+        else:
+            matchResult.loc[0, "label"] = "Draw"
+        return matchResult.loc[0]
 
 
 class TeamDataHelper(DataHelper):
@@ -48,6 +111,39 @@ class PlayerDataHelper(DataHelper):
         return self.data.loc[self.data.player_api_id == id].player_name.values[0]
 
 
+class PlayerAttributeDataHelper(DataHelper):
+    def getPlayerRatings(self, match):
+        players = list()
+        for i in range(1, 12):
+            players.append("home_player_{}".format(i))
+            players.append("away_player_{}".format(i))
+
+        playerRatings = pd.DataFrame()
+        for player in players:
+            playerStats = self.data[self.data.player_api_id == match[player]]
+            playerStats = playerStats[
+                playerStats.date < match["date"]].sort_values(
+                    by="date", ascending=False)[:1]
+
+            if np.isnan(match[player]):
+                overallPlayerRating = pd.Series(0)
+            else:
+                playerStats.reset_index(inplace=True, drop=True)
+                overallPlayerRating = pd.Series(
+                    playerStats.loc[0, "overall_rating"])
+
+            playerRatings = pd.concat(
+                [playerRatings, overallPlayerRating], axis=1)
+
+        columnNames = []
+        for i in range(1, 12):
+            columnNames.append("home_player_{}_overall_rating".format(i))
+            columnNames.append("away_player_{}_overall_rating".format(i))
+        playerRatings.columns = columnNames
+        playerRatings["match_api_id"] = match.match_api_id
+        return playerRatings.iloc[0]
+
+
 class MatchResultPredictDataAggregator(object):
     def __init__(self, database):
         self.database = database
@@ -56,10 +152,15 @@ class MatchResultPredictDataAggregator(object):
         self.leagueData = self.database.runQuery("SELECT * FROM League;")
         self.teamData = self.database.runQuery("SELECT * FROM Team;")
         self.playerData = self.database.runQuery("SELECT * FROM Player;")
+        self.playerAttributeData = self.database.runQuery(
+            "SELECT * FROM Player_Attributes")
         self.countryDataHelper = CountryDataHelper(self.countryData)
         self.leagueDataHelper = LeagueDataHelper(self.leagueData)
         self.teamDataHelper = TeamDataHelper(self.teamData)
         self.playerDataHelper = PlayerDataHelper(self.playerData)
+        self.matchDataHelper = MatchDataHelper(self.matchData)
+        self.playerAttributeDataHelper = PlayerAttributeDataHelper(
+            self.playerAttributeData)
         self.aggregatedData = copy.deepcopy(self.matchData)
 
     def addCountryNameToMatches(self):
